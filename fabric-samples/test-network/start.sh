0baf152a1b12648ea3802c3b04a04e79ecb2a232
Lea Kalmar <lea.kalmar@gmail.com>
2023-12-19 08:47:02 +0100

    feat: add all fabric-samples needed

---
+++ fabric-samples/test-network/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+./network.sh "down"
+#./network.sh "up" "createChannel" "-c" "mychannel" "-ca"
+#./network.sh "deployCC" "-ccn" "basic" "-ccp" "../asset-transfer-basic/chaincode-typescript/" "-ccl" "typescript"
+
+./network.sh "up" "createChannel" "-s" "couchdb" "-c" "mychannel" "-ca"
+./network.sh "deployCC" "-ccn" "ledger" "-ccp" "../asset-transfer-ledger-queries/chaincode-go/" "-ccl" "go"
+
+cp -r /home/leakalmar/hyperledger-fabric/test-network/organizations/peerOrganizations/org1\.example\.com /mnt/e/GIT/HealthscapeOrganization/OrganizationApp/src/main/resources
